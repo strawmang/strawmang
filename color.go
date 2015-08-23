6d92f9728393685c4a2c76a3c567a1db09b2c28b
Nick Powell <markusthefish@gmail.com>
2015-08-22 23:05:19 -0700

    Implemeneted basic batch color pallete allocation for unique username coloring

---
+++ color.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"github.com/lucasb-eyer/go-colorful"
+	"log"
+)
+
+var colors = []colorful.Color{}
+
+// PopColor returns a new color from our color stack.  If we run
+// out of colors and fail to allocate a new batch we will return
+// an error
+func PopColor() colorful.Color {
+	if len(colors) == 0 {
+		AllocateColors()
+	}
+	var color colorful.Color
+	color, colors = colors[len(colors)-1], colors[:len(colors)-1]
+	return color
+}
+
+// AllocateColors returns a fresh batch of colors
+func AllocateColors() {
+	log.Printf("Allocating new generateColor batch")
+	colors = colorful.FastHappyPalette(1000)
+}
+
+// TODO: This
+func generateColor() string {
+	// colorful.FastWarm
+	return "000000"
+}
---
+++ color_test.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestColors(t *testing.T) {
+	for i := 0; i < 1200; i++ {
+		t.Log("Generating color", i)
+		color := PopColor()
+		t.Logf("Generated color: %v", color.Hex())
+	}
+}
