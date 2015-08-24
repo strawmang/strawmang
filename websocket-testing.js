4e1af37be709f4f3ee3f9845d0d291f85ede3868
Nick Powell <markusthefish@gmail.com>
2015-08-24 14:45:47 -0700

    1337 frontend skillz

--- websocket-testing.js
+++ websocket-testing.js
@@ -5,20 +5,44 @@ function onmessage(event) {
   console.log(event.data);
   var data = JSON.parse(event.data);
   switch (data.type) {
+    case "error":
+      handleError(data.error);
+      break;
     case "message":
-      var area = document.getElementById("chat");
-      area.innerHTML = area.innerHTML + '<span style="color: #'+data.color+';">'+data.username+'</span>:  '+data.text+'<br>';
+      var chat = $("#chat");
+      var topic = null;
+      $.each(topics, function(key, value) {
+        if (value.id == data["topic-id"]) {
+          console.log("match!");
+          topic = value;
+        }
+      });
+      
+      var topicName = "";
+      if (topic === null) {
+        topicName = "GLOBAL";
+      } else {
+        console.log(topic);
+        topicName = topic["option-a"] + ' vs. ' + topic["option-b"];
+      }
+      chat.append('[' + topicName + ']' +
+        '<span style="color: #'+data.color+';">'+data.username+'</span>:  '+data.text+'<br>');
       break;
     case "status":
       break;
   }
 }
 
-function onerror(event) {
-  console.log(event.data);
+function handleError(error) {
+  $("#chat").append('[<span style="color: red;">ERROR</span>] ' + error); 
+}
+
+function onerror(err) {
+  console.log(err);
 }
 
 var socket = new WebSocket("ws://localhost:8080/ws");
+var topics = {};
 
 socket.onmessage = onmessage;
 socket.onerror = onerror;
@@ -43,20 +67,22 @@ function login() {
 
 function sendMessage() {
   switch (socket.readyState) {
-  case 0:
-    console.log("That connetion isn't open yet!");
-    return;
-  case 1:
-    break;
-  case 2:
-    console.log("That socket is closing!");
-    return;
-  case 3:
-    console.log("That socket is closed!");
-    return;
+    case 0:
+      console.log("That connetion isn't open yet!");
+      return;
+    case 1:
+      break;
+    case 2:
+      console.log("That socket is closing!");
+      return;
+    case 3:
+      console.log("That socket is closed!");
+      return;
   }
   var text = document.getElementById("text").value;
-  var data = JSON.stringify({type:"message", text:text});
+  var sid = $("#topics-sel").val();
+  var id = parseInt(sid);
+  var data = JSON.stringify({type:"message", text:text, "topic-id": id});
   socket.send(data);
 }
 
@@ -65,18 +91,35 @@ function leave() {
   socket.send(data);
 }
 
+function newTopic() {
+  var a = $("#option-a").val();
+  var b = $("#option-b").val();
+
+  var data = {type:"newtopic"};
+  data["option-a"] = a;
+  data["option-b"] = b;
+  socket.send(JSON.stringify(data));
+}
+
 function getTopics() {
   var req = new XMLHttpRequest();
   req.open("GET", "http://localhost:8080/status", true);
   req.send(null);
 
-  req.onload = function() {
+  req.onload = function() { 
     var data = JSON.parse(req.responseText);
+    topics = data.topics;
     console.log(req.responseText);
-    var topics = document.getElementById("topics");
-    topics.innerHTML = "";
+    topicsElem = document.getElementById("topics");
+    topicsElem.innerHTML = "";
     for (var i = 0; i < data.topics.length; i++) {
-      topics.innerHTML = topics.innerHTML + '<p>ID: ' + data.topics[i].id + ' ' + data.topics[i].option-a + ' vs. ' + data.topics[i].option-b;
+      topicsElem.innerHTML = topicsElem.innerHTML + '<p>ID: ' + data.topics[i].id + ' ' + data.topics[i]["option-a"] + ' vs. ' + data.topics[i]["option-b"];
     }
+    $("#topics-sel").find("option").remove();
+    $("#topics-sel").append('<option value="" disabled selected>Select your option</option>');
+    $.each(data.topics, function(key, value) {
+      $("#topics-sel").append('<option value="'+value.id+'">'+
+          value["option-a"] +" vs. " + value["option-b"])
+    })
   }
 }
