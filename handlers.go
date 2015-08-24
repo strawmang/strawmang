2a93f0cd4ba8fcf5888208105e22ccb79bd621a0
Nick Powell <markusthefish@gmail.com>
2015-08-24 08:38:46 -0700

    Move handlers to their own file

---
+++ handlers.go
@@ -0,0 +1,21 @@
+package main
+
+// TODO: Not production ready.  Needs to save the index in memory and only reload it if the file changes
+func handlerIndex(rw http.ResponseWriter, req *http.Request) {
+	data, err := ioutil.ReadFile("index.html")
+	if err != nil {
+		// TODO: Pretty 503
+		rw.WriteHeader(http.StatusInternalServerError)
+		rw.Write([]byte(err.Error()))
+		return
+	}
+	rw.Write(data)
+}
+func handlerWs(rw http.ResponseWriter, req *http.Request) {
+	conf, err := websocket.NewConfig("ws://localhost", "http://localhost")
+	if err != nil {
+		log.Printf("Websocket: %v\n", err.Error())
+	}
+	s := websocket.Server{Handler: websocket.Handler(chat.HandlerChat), Config: *conf}
+	s.ServeHTTP(rw, req)
+}
